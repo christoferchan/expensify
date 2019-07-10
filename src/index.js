535f7f7caed76c34a5b631320643cc357945f125
github username <github email address>
2019-07-10 04:39:53 -0400

    test

--- src/index.js
+++ src/index.js
@@ -5,27 +5,17 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expeneses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
-const ConnectedApp = (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
 
 console.log(store.getState());
 
-store.dispatch(addExpense({ description: "water bill", amount: 4500 }));
-store.dispatch(addExpense({ description: "gas bill", createdAt: 1000 }));
-store.dispatch(addExpense({ description: "rent", amount: 10000 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
-ReactDOM.render(<ConnectedApp />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
