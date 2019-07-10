dea1eb2b582f23b6fc99681a77e4266ea867ba81
github username <github email address>
2019-07-10 04:43:34 -0400

    changed routes

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "../public");
+const publicPath = path.join(__dirname, "..", "build");
 
 app.use(express.static(publicPath));
 app.get("/ping", function(req, res) {
   return res.send("pong");
 });
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 app.listen(port, () => {
